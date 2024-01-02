621aa85aee0e018ccec11d641f4bab5f94b07b3a
Xinyuan Luo <lux1lo@MacBook-Pro-von-Xinyuan.fritz.box>
2024-01-02 19:47:20 +0100

    getDataCode

---
+++ neu.py
@@ -0,0 +1,60 @@
+# Importieren der PyTorch-Bibliotheken.
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import pickle
+import torch.nn as nn
+import torch.optim as optim
+import torch.distributed as dist
+
+
+# Transformations-Pipeline für die Bilder des CIFAR-100 Datensatzes.
+# 'transforms.Compose' kombiniert mehrere Transformationsschritte.
+transform = transforms.Compose([
+    transforms.ToTensor(),  
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  #
+])
+# Laden des CIFAR-100 Trainingssatzes. 'train=True' wählt den Trainingssatz aus.
+trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
+
+# Laden des CIFAR-100 Testsatzes. 'train=False' wählt den Testsatz aus.
+testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
+
+# Liste der Label-Indizes für die Superklasse 'aquatic mammals' im CIFAR-100 Datensatz.
+aquatic_mammals_labels = [4, 30, 55, 72, 95] 
+
+# Eine Funktion, die die Daten des Datensatzes filtert, um nur die Einträge der 'aquatic mammals' Superklasse zu behalten.
+#und die Datensätze in einem File speichert
+def filter_aquatic_mammals_test(dataset, labels):
+    class_indices = [i for i, label in enumerate(dataset.targets) if label in labels]
+    dataset.data = dataset.data[class_indices]
+    dataset.targets = [dataset.targets[i] for i in class_indices]
+    with open('test_aquatic_data.pkl', 'wb') as file:
+        pickle.dump(dataset, file)
+
+def filter_aquatic_mammals_train(dataset, labels):
+    class_indices = [i for i, label in enumerate(dataset.targets) if label in labels]
+    dataset.data = dataset.data[class_indices]
+    dataset.targets = [dataset.targets[i] for i in class_indices]
+    with open('train_aquatic_data.pkl', 'wb') as file:
+        pickle.dump(dataset, file)
+
+# Anwendung der Filterfunktion auf den Trainings- und Testsatz.
+filter_aquatic_mammals_train(trainset, aquatic_mammals_labels)
+filter_aquatic_mammals_test(testset, aquatic_mammals_labels)
+
+#save data in file 
+with open('test_aquatic_data.pkl', 'rb') as file:
+    testset = pickle.load(file)
+
+with open('train_aquatic_data.pkl', 'rb') as file:
+    trainset = pickle.load(file)
+
+
+
+#get saved data
+# Erstellen eines DataLoader-Objekts für den Testsatz.
+# 'shuffle=False' wird hier typischerweise gesetzt, da beim Testen die Reihenfolge der Daten nicht wichtig ist.
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False)
